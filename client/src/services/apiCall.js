7c0be6695471d1256e3f78ddf8a4f1b32b0f6321
unknown <ramaiahrajg@gmail.com>
2018-09-29 20:31:43 +0530

    Fix

--- client/src/services/apiCall.js
+++ client/src/services/apiCall.js
@@ -1,44 +1,37 @@
-const apiCall = (route, method='GET', body={}) => {
-    return new Promise((resolve, reject) => {
+const apiCall = (route , method='GET',body={}) => {
 
+    return new Promise((resolve , reject) =>{
         const headers = new Headers({
             'Content-Type': 'application/json',
         });
-
         const requestDetails = {
             method,
-            mode: 'cors',
+            mode : 'cors',
             headers,
-        };
-
-        if(method !== 'GET') {
+    
+        }
+    
+        if (method !== 'GET') {
             requestDetails.body = JSON.stringify(body);
         }
-
-        const serverUrl = "http://127.0.0.1:10010";
-
-        const handleErrors = response => {
-            if(response.ok) {
+    
+        const serverUrl = 'http://127.0.0.1:10010';
+    
+        const handleErrors= response => {
+            if (response.ok) {
                 return response.json();
-            } else {
-                throw Error(response.statusText);
             }
-        };
-
-        fetch(`${serverUrl}/${route}`, requestDetails)
-            .then(handleErrors)
-            .then(data => resolve(data))
-            .catch(err => reject(err));
-
-        // .then(response => response.json())
-        // .then(data => {
-        //     console.log(data);
-        // })
-        // .catch(err => {
-        //     console.log(err);
-        // })
-
+            else {
+                throw Error(response.statusTxt);
+            }
+        }
+    
+        fetch(`${serverUrl}/${route}`,requestDetails)
+        .then(handleErrors)
+        .then(data => resolve(data))
+        .catch(err=> reject(err))
+   
+    
     });
-};
-
+}
 export default apiCall;
